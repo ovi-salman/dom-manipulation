34dff7eadf671c753bdbf14bbaf09bd4cef7bfd8
salman <ovvi@outlook.com>
2018-05-30 04:16:05 +0000

    push

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ console.log(heading.innerText)
 
 /* Task 2 using plain JavaScript */
 
+var textBtn = document.getElementById('textBtn2')
+textBtn.addEventListener('click',function(){
+    var task2 =document.getElementById('task2a')
+    var paragraph = document.createElement('p')
+    paragraph.innerText = "Hellow World"
+    task2.appendChild(paragraph)
+    
+})
 
 
 /* Task 4 using jQuery */
